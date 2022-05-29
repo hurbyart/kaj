ab0f23974c9b30c3fcc40a61979f96b26d0e6b0b
Artem Hurbych <hurbyart@fel.cvut.cz>
2022-05-29 23:40:56 +0200

    m

--- noteme/node_modules/ng-onload/test/test.js
+++
@@ -1,8 +0,0 @@
-// Declaring the stub application to inject our directive into
-angular
-	.module("test", [ "ngOnload" ])
-	.controller("TestController", function($scope) {
-		$scope.helloTest = function(contentLocation) {
-			console.log("Hello world!", contentLocation);
-		};
-	});

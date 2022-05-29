5625e32321396d921c3534dc068fe8cdb0783b8c
Artem Hurbych <hurbyart@fel.cvut.cz>
2022-05-29 23:11:32 +0200

    deploy commit

---
+++ noteme/src/app/noteme/todo.ts
@@ -0,0 +1,12 @@
+//Todo class
+export class Todo {
+    // id: number;
+    // todo: string;
+
+    constructor(
+      public text:string,
+      public id:number,
+      public date:number=Date.now(),
+      public deadline:number,
+    ){}
+  }
